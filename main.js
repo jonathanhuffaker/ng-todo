7896cba8cd1ccf1ebf246797a452d4d4bebc251f
Jon Huffaker <jonathanhuffaker@gmail.com>
2015-11-09 19:44:14 -0600

    second commit

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-var app = angular.module("TodoApp",[]);
+var app = angular.module('TodoApp', []);
 
-app.controller("TodoCtrl", function($scope){
-	$scope.title= "Welcome to your Todo App";
-	$rootScope.hello= "I am everywhere";
-})
+app.controller("TodoCtrl", function($scope, $rootScope){
+  $scope.title = "Welcome to your Todo app";
+  $rootScope = "I AM EVERYWHERE!";
+
+  $scope.todos = [
+  {name: "Mow the lawn"},
+  {name: "kill the cat"},
+  {name: "eat more beef"},
+  {name: "taxes and bitches"}
+  ]
+});
+
+
 
 app.controller("PastaCtrl", function($scope){
-	$scope.pasta = "I love carbs";
+  $scope.pasta = "Yay Pasta!";
+  $scope.foods = "";
 })
